d81f4d475c74de2b1392416acbe5ca25ce0b0c4d
Nicolas <nicolas-kik@nicolas-kik.com>
2019-05-23 11:34:40 +0300

    Comments

--- app/src/main/java/com/example/movienator/TMDbApi.java
+++ app/src/main/java/com/example/movienator/TMDbApi.java
@@ -8,6 +8,8 @@ import retrofit2.http.Query;
 public interface TMDbApi {
 
 
+    /* We use Call instead of List for asynchronous http get requests
+    *  The return type is MoviesResponse which is defined in MoviesResponse.java*/
     @GET("movie/popular")
     Call<MoviesResponse> getPopularMovies(
             @Query("api_key") String apiKey,
@@ -29,6 +31,7 @@ public interface TMDbApi {
             @Query("page") int page
     );
 
+    /*  The return type is a list of GenresResponse which is defined in GenresResponse.java*/
     @GET("genre/movie/list")
     Call<GenresResponse> getGenres(
             @Query("api_key") String apiKey,
@@ -36,6 +39,7 @@ public interface TMDbApi {
     );
 
     // https://api.themoviedb.org/3/movie/{movie_id}?api_key=39c69a10c366d0d25de6fec34b2cf1bc&language=en-US
+    /*  The return type is a Movie which is defined in Movie.java*/
     @GET("movie/{movie_id}")
     Call<Movie> getMovie(
             @Path("movie_id") int id,
